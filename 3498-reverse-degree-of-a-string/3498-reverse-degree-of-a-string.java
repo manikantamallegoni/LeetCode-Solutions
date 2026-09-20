9a0f774dd29663aba2b77097ccbb01392f82eff1
manikantamallegoni <235069059+manikantamallegoni@users.noreply.github.com>
2026-09-20 23:18:07 +0530

    Time: 1 ms (100%), Space: 43.8 MB (81.16%) - LeetHub

--- 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.java
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.java
@@ -1,18 +1,19 @@
 class Solution {
     public int reverseDegree(String s) {
-        int ans=0;
-      ans= multiRe(s,ans);
-       return ans;
-        
-        
+        return multiRe(s);
     }
-    int multiRe(String s,int ans){
-          for(int i=0;i<s.length();i++){
-            char c=s.charAt(i);
-            int val='z'-c+1;
-             ans+= val*(i+1);
-    }
-    return ans;
-}
 
+    int multiRe(String s) {
+        int ans = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            int val = 'z' - c + 1;
+
+            ans += val * (i + 1);
+        }
+
+        return ans;
+    }
 }
